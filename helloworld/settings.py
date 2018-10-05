055baea4cf87aeab593d4eba1e3968c20b872433
spark131072 <spark131072@gmail.com>
2018-10-05 14:40:53 +0800

    Initial commit

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -47,6 +47,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+MIDDLEWARE_CLASSES = [
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
